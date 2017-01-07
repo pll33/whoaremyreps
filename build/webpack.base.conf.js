0a6020362902ba76ce04eb728cd14c4681106697
Peter Liu <Peterljr888@gmail.com>
2017-01-06 22:12:22 -0500

    update .gitignore and build config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -77,6 +77,10 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?.*)?$/,
+        loader: 'file'
       }
     ]
   },
