c6ee13e08f8c25ec961a5b71049cb747622e6f4f
Peter Liu <Peterljr888@gmail.com>
2017-02-22 04:04:57 -0500

    rename RepresentativeLevel to RepresentativeList, added ClipLoader

---
+++ src/views/RepresentativeList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="rep-list" v-bind:class="'rep-list-'+level">
+    <representative-item v-for="rep in repsData" :rep="rep"></representative-item>
+    <clip-loader v-if="!repsData" :loading="loading" :color="loader.color" :size="loader.size"></clip-loader>
+  </div>
+</template>
+
+<script>
+import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
+import RepresentativeItem from '../components/RepresentativeItem'
+
+export default {
+  name: 'representative-list',
+  components: {
+    ClipLoader, RepresentativeItem
+  },
+  mounted: function () {
+    // return to /all if nothing loads
+  },
+  data: function () {
+    return {
+      loader: {
+        color: '#2C3E50'
+      }
+    }
+  },
+  computed: {
+    repsData () {
+      return this.$store.getters.getRepresentativesByLevel
+    }
+  }
+}
+</script>
+
+<style>
+</style>
