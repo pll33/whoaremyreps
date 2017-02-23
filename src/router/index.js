cd85fdd0244a5d8d8acb657ec5591874eeb8c86b
Peter Liu <Peterljr888@gmail.com>
2017-02-23 01:18:10 -0500

    update router to reflect newly added views

--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,26 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-import Home from '../views/Home'
+// import Home from '../views/Home'
 import NotFound from '../views/NotFound'
+import About from '../views/About'
 import PrivacyPolicy from '../views/PrivacyPolicy'
-import RepresentativeLevel from '../views/RepresentativeLevel'
+import LegislativeComposition from '../views/LegislativeComposition'
+import AdditionalResources from '../views/AdditionalResources'
+import RepresentativeList from '../views/RepresentativeList'
 import RepresentativePage from '../views/RepresentativePage'
 
 export default new Router({
   mode: 'history',
   base: __dirname,
   routes: [
-    { path: '/', name: 'home', component: Home },
-    { path: '/privacy', component: PrivacyPolicy },
-    { path: '/:level', component: RepresentativeLevel },
-    { path: '/:level/:rep', component: RepresentativePage },
-    // { path: '/:level/:rep/print', component: PrintRepresentative },
+    { path: '/', name: 'home' },
+    { path: '/about', name: 'about', component: About },
+    { path: '/privacy', name: 'privacy', component: PrivacyPolicy },
+    { path: '/composition', name: 'composition', component: LegislativeComposition },
+    { path: '/resources', name: 'resources', component: AdditionalResources },
+    { path: '/:level(all|federal|state|local)', component: RepresentativeList },
+    { path: '/:level(all|federal|state|local)/:rep', component: RepresentativePage },
     { path: '*', component: NotFound }
   ]
 })
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,20 @@
 <template>
-  <span>404 Not Found</span>
+  <div class="page">
+    <h2>404 Not Found</h2>
+    <loader-redirect :timeout="5000" color="#3C4E6F"></loader-redirect>
+    <span>Redirecting in 5 seconds...</span>
+  </div>
 </template>
+
+<script>
+import LoaderRedirect from '../components/LoaderRedirect'
+
+export default {
+  name: 'not-found',
+  components: { LoaderRedirect },
+  beforeRouteLeave (to, from, next) {
+    console.log(to, from)
+    next()
+  }
+}
+</script>
