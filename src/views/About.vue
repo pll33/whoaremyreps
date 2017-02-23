00ac3e8a09a9008cb93b2582dbf8dc324369304f
Peter Liu <Peterljr888@gmail.com>
2017-02-23 01:13:27 -0500

    basic setup for About view

---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <!--
+    made with Civic Information Project, Vuejs
+    APIs: ProPublica, OpenStates, OpenSecrets
+
+    info@whoaremyreps.com
+
+    donate / tip
+
+   -->
+  <div>
+    <h2>About</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style>
+</style>
