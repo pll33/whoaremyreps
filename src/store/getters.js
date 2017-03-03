c0be185dccc9ed3fa2e5acebc96166d7c86dd632
Peter Liu <Peterljr888@gmail.com>
2017-03-03 06:51:33 -0500

    move all getters into one single export

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,43 +1,45 @@
-// export const userGeolocation = state => state.locationData.position
-
-// export const userAddress = state => state.locationData.address
-
-// export const getVCard =
-
-export const getRepresentatives = (state) => {
-  return state.apiData.representatives
-}
-
-export const getRepresentativePage = (state) => {
-  return state.apiData.representatives[state.route.params.rep]
-}
-
-export const getRepresentativeSocialMedia = (state) => {
-  if (state.route.params.rep) {
-    return state.apiData.representatives[state.route.params.rep].channels
+export default {
+  getRepresentatives (state) {
+    return state.apiData.representatives
+  },
+  getRepresentativePage (state) {
+    return state.apiData.representatives[state.route.params.rep]
+  },
+  getRepresentativeSocialMedia (state) {
+    if (state.route.params.rep) {
+      return state.apiData.representatives[state.route.params.rep].channels
+    }
+  },
+  getRepresentativeInfo (state) {
+    if (state.route.params.rep) {
+      return state.apiData.representatives[state.route.params.rep].info
+    }
+  },
+  getRepresentativesByLevel (state) {
+    return state.apiData.levels[state.route.params.level]
+  },
+  getLegislativeComposition (state) {
+    return state.apiData.composition
+  },
+  getPresidentParty (state) {
+    let execs = state.apiData.levels.execs
+    if (execs) {
+      return state.apiData.representatives[execs.president].party
+    }
+  },
+  getGovernorParty (state) {
+    let execs = state.apiData.levels.execs
+    if (execs) {
+      return state.apiData.representatives[execs.governor].party
+    }
+  },
+  getVCard (state) {
+    // TO-DO
+  },
+  getUserAddress (state) {
+    return state.locationData.position
+  },
+  getUserGeolocation (state) {
+    return state.locationData.address
   }
-}
-
-export const getRepresentativesByLevel = (state) => {
-  return state.apiData.levels[state.route.params.level]
-}
-
-export const getLegislativeComposition = (state) => {
-  return state.apiData.composition
-}
-
-export const getPresidentParty = (state) => {
-  return state.apiData.levels.federal.filter(rep => rep.position.indexOf('President') !== -1)[0].party
-}
-
-export const getGovernorParty = (state) => {
-  return state.apiData.levels.state.filter(rep => rep.position === 'Governor')[0].party
-}
-
-export const getUserAddress = (state) => {
-  return state.locationData.position
-}
-
-export const getUserGeolocation = (state) => {
-  return state.locationData.address
 }
