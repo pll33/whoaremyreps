8da03560c795facdc6fc595520061534bf581cd8
Peter Liu <Peterljr888@gmail.com>
2017-03-12 11:01:03 -0400

    minor edit: fix spacing

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -42,7 +42,7 @@
       <p>We do not knowingly collect personally identifiable information from children under 13. If you are a parent or guardian and you learn that your Children have provided us with Personal Information, please contact us. If we become aware that we have collected Personal Information from a children under age 13 without verification of parental consent, we take steps to remove that information from our servers.</p>
 
       <h3>Changes To This Privacy Policy</h3>
-      <p>This Privacy Policy is effective as of {{ lastUpdatedDate }}and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.</p>
+      <p>This Privacy Policy is effective as of {{ lastUpdatedDate }} and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.</p>
       <p>We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. Your continued use of the Service after we post any modifications to the Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide and be bound by the modified Privacy Policy.</p>
       <p>If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.</p>
 
