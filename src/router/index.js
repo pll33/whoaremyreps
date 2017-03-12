68c4fe746ef2546c3a2f73d304e6ea4e472b5715
Peter Liu <Peterljr888@gmail.com>
2017-03-12 10:42:36 -0400

    add icon legend to about page, remove additional resources page, update router

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,6 @@ import NotFound from '../views/NotFound'
 import About from '../views/About'
 import PrivacyPolicy from '../views/PrivacyPolicy'
 import LegislativeComposition from '../views/LegislativeComposition'
-import AdditionalResources from '../views/AdditionalResources'
-import IconLegend from '../views/IconLegend'
 import RepresentativeList from '../views/RepresentativeList'
 import RepresentativePage from '../views/RepresentativePage'
 
@@ -20,8 +18,6 @@ export default new Router({
     { path: '/about', name: 'about', component: About },
     { path: '/privacy', name: 'privacy', component: PrivacyPolicy },
     { path: '/composition', name: 'composition', component: LegislativeComposition },
-    { path: '/resources', name: 'resources', component: AdditionalResources },
-    { path: '/legend', name: 'legend', component: IconLegend },
     { path: '/:level(all|federal|state|local)', component: RepresentativeList },
     { path: '/:level(all|federal|state|local)/:rep', component: RepresentativePage },
     { path: '*', component: NotFound }
--- src/views/IconLegend.vue
+++ src/views/IconLegend.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="container">
-    <h2>Icon Legend <span class="heading-desc">(parties with elected officials)</span></h2>
+  <div>
+    <h3>Icon Legend <span class="heading-desc">(parties with elected officials)</span></h3>
     <ul class="legend">
       <li><party-affiliation-marker party="Democratic"></party-affiliation-marker>Democratic</li>
       <li><party-affiliation-marker party="Republican"></party-affiliation-marker>Republican</li>
+      <li><party-affiliation-marker party="Independent"></party-affiliation-marker>Independent</li>
       <li><party-affiliation-marker party="Libertarian"></party-affiliation-marker>Libertarian</li>
       <li><party-affiliation-marker party="Green"></party-affiliation-marker>Green</li>
       <li><party-affiliation-marker party="Socialist"></party-affiliation-marker>Socialist</li>
       <li><party-affiliation-marker party="Progressive/Democratic"></party-affiliation-marker>Vermont Progressive</li>
-      <li><party-affiliation-marker party="Independent"></party-affiliation-marker>Independent</li>
       <li><party-affiliation-marker party="Unknown"></party-affiliation-marker>Unknown</li>
-      <!-- constitution party, -->
     </ul>
   </div>
 </template>
