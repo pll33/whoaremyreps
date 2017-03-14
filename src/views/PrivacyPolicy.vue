919becafbb8d37835f05ee7c6c84d0f9771b3f5a
Peter Liu <Peterljr888@gmail.com>
2017-03-14 16:48:34 -0400

    update copy for privacypolicy

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -46,7 +46,7 @@
     <h3>Changes To This Privacy Policy</h3>
     <p>This Privacy Policy is effective as of {{ lastUpdatedDate }} and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.</p>
     <p>We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. Your continued use of the Service after we post any modifications to the Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide and be bound by the modified Privacy Policy.</p>
-    <p>If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.</p>
+    <p>If we make any material changes to this Privacy Policy, we will notify you by placing a prominent notice on our website.</p>
 
     <p class="text-bold">Contact Us</p>
 
