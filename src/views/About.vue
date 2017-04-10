d35dc1b32a59f4701e6673366d3a7f0b9a76c5e5
Peter Liu <Peterljr888@gmail.com>
2017-04-10 12:34:42 -0400

    update copy

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h2>About</h2>
 
-    <p>WhoAreMyReps.com is a resource that displays your elected officials at all levels of U.S. government.</p>
+    <p>WhoAreMyReps.com is a resource that shows your elected officials at all levels of U.S. government.</p>
     <p>Information regarding legislative or executive actions can be found on specific pages marked with an info icon <i class="fa fa-info-circle" style="color:#444;"></i>.</p>
     <p>Representative data comes from a number of APIs: <br><a href="https://developers.google.com/civic-information/" target="_blank" rel="noopener">Google Civic Information</a>, <a href="https://sunlightlabs.github.io/congress/" target="_blank" rel="noopener">Sunlight Congress</a>, <a href="https://propublica.github.io/congress-api-docs/" target="_blank" rel="noopener">ProPublica Congress</a>, <a href="http://docs.openstates.org/en/latest/api/" target="_blank" rel="noopener">OpenStates</a>, and <a href="https://www.federalregister.gov/reader-aids/developer-resources/rest-api" target="_blank" rel="noopener">Federal Register</a>.</p>
     <p>Questions or concerns? Let us know at <a href="mailto:hello@whoaremyreps.com">hello@whoaremyreps.com</a>.</p>
