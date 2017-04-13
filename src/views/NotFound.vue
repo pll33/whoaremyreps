f1bc797e7ed765549f44e594fdaef18e24536414
Peter Liu <Peterljr888@gmail.com>
2017-04-13 14:48:38 -0400

    update view

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <h2>404 Not Found</h2>
-    <loader-redirect :timeout="5000" color="#455A80"></loader-redirect>
+    <h2>404 Page Not Found</h2>
     <span>Redirecting to <router-link to="/all">/all</router-link> in 5 seconds...</span>
+    <loader-redirect :timeout="5000" color="#455A80"></loader-redirect>
   </div>
 </template>
 
