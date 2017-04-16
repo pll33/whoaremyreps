5fd05ccbd84d4d34e5c38af69044289e400f2abf
Peter Liu <Peterljr888@gmail.com>
2017-04-15 20:37:10 -0400

    add email link component

---
+++ src/components/EmailLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <a class="hidemail" href="#" :data-alias="alias" v-on:click="showEmail"></a>
+</template>
+<script>
+export default {
+  name: 'email-link',
+  props: ['alias'],
+  methods: {
+    showEmail () {
+      window.location.href = 'mailto:' + this.alias + '@whoaremyreps.com'
+    }
+  }
+}
+</script>
+<style>
+.hidemail:after {
+  content: attr(data-alias) "@whoaremyreps.com";
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -5,8 +5,8 @@
     <p>WhoAreMyReps.com is a resource that shows your elected officials at all levels of U.S. government.</p>
     <p>Information regarding legislative or executive actions can be found on specific pages marked with an info icon <i class="fa fa-info-circle" style="color:#444;"></i>.</p>
     <p>Representative data comes from a number of APIs: <br><a href="https://developers.google.com/civic-information/" target="_blank" rel="noopener">Google Civic Information</a>, <a href="https://sunlightlabs.github.io/congress/" target="_blank" rel="noopener">Sunlight Congress</a>, <a href="https://propublica.github.io/congress-api-docs/" target="_blank" rel="noopener">ProPublica Congress</a>, <a href="http://docs.openstates.org/en/latest/api/" target="_blank" rel="noopener">OpenStates</a>, and <a href="https://www.federalregister.gov/reader-aids/developer-resources/rest-api" target="_blank" rel="noopener">Federal Register</a>.</p>
-    <p>Questions or concerns? Let us know at <a href="mailto:hello@whoaremyreps.com">hello@whoaremyreps.com</a>.</p>
-    <p>If you encounter any mistakes or errors, please email <a href="mailto:errors@whoaremyreps.com">errors@whoaremyreps.com</a>.</p>
+    <p>Questions or concerns? Let us know at <email-link alias="hello"></email-link>.</p>
+    <p>If you encounter any mistakes or errors, please email <email-link alias="errors"></email-link>.</p>
 
     <icon-legend></icon-legend>
 
@@ -17,10 +17,11 @@
   </div>
 </template>
 <script>
+import EmailLink from '../components/EmailLink'
 import IconLegend from '../views/IconLegend'
 
 export default {
   name: 'home',
-  components: { IconLegend }
+  components: { EmailLink, IconLegend }
 }
 </script>
